f3b5af2e80f0520e4cc7abb448d9f42fb8e10f18
Cameron McHenry <camchenry@users.noreply.github.com>
2021-05-23 18:38:25 -0400

    Replace favicon

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,7 @@ function Document({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
