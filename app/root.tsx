fc9f7e24a5aa81418d36947fcb12495e90379b66
Cameron McHenry <camchenry@users.noreply.github.com>
2021-09-19 22:44:24 -0400

    Add tools page to header for easy navigation

--- app/root.tsx
+++ app/root.tsx
@@ -53,8 +53,11 @@ export default function App() {
               <span className="font-bold">Cameron McHenry</span>
             </a>
           </li>
-          <li>
+          <li className="mr-4">
             <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/tools">Tools</a>
           </li>
         </ul>
       </nav>
