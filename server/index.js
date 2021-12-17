915a83c2c1fa85272f7e0ce5525da68cc48f7a7a
Cameron McHenry <camchenry@users.noreply.github.com>
2021-12-16 20:32:53 -0500

    Try LD_LIBRARY_PATH thing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 const { createRequestHandler } = require("@remix-run/vercel");
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 module.exports = createRequestHandler({
   build: require("./build"),
 });
