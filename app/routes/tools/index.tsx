85aeff9083ea989d0801f554db98c5375292128b
Cameron McHenry <camchenry@users.noreply.github.com>
2023-07-27 19:35:11 -0400

    Add CacheCheck

--- app/routes/tools/index.tsx
+++ app/routes/tools/index.tsx
@@ -14,6 +14,13 @@ export default function ToolsIndex() {
       <ul className="flex flex-col space-y-4">
         <li>
           <PageCard
+            title="CacheCheck"
+            summary="Check the HTTP caching headers for any website. Type in a URL to learn the details of how the HTTP caching for the page works and whether it will be cached or not."
+            url="https://cachecheck.net"
+          />
+        </li>
+        <li>
+          <PageCard
             title="Is It Worth It To Automate?"
             summary="This calculator helps to determine whether it saves times or wastes time to prematurely optimize a process with automation."
             url="/tools/worth-it-to-automate"
